26d3d4dbd92b1291df03657867df5e3cbe571f6a
frostyral <csjjgarcia@gmail.com>
2022-10-21 14:55:21 +0800

    Added Wagtail Admin Dashboard System

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-6$ndfiabg%6z2kcyk!ch!kxbmo&tb%sak%iw0bk^z69^4apymb"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
